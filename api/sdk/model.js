af2d18ff8f20610914ca2a359cfd3bf0ca709969
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-07-16 00:10:30 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Islahuddin41420110058/UAS41420110058/main/api/sdk/model/model.json';
+        const path = 'https://raw.githubusercontent.com/Islahuddin41420110058/UASKEDUA/main/api/sdk/model/model%20(1).json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
